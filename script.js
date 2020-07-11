2e82a6f810bf8db4f63dc6bdc91092c83c4fd51c
Wildelyssa <wildelyssa@gmail.com>
2020-07-11 14:20:04 +0100

    adding files

---
+++ script.js
@@ -0,0 +1,13 @@
+//var fname = document.getElementByID("fname").value;
+//var lname = document.getElementByID("lname").value;
+//var email = document.getElementByID("email").value;
+//var subject = document.getElementByID("subject").value;
+//var submitBtn = document.getElemetByID("submitBtn");
+
+//add event listener
+//Onclick, collect data from the contact form, make it into an object and email it to my email address
+
+//Additional
+//Check the email address is valid
+//Check none of the values are left empty
+
